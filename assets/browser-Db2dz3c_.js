9f54b024eae8914aee44a0011e1be7accfb9a621
Jefferson <hbrcomercialssa@gmail.com>
2025-06-05 00:12:05 -0300

    Updates

---
+++ assets/browser-Db2dz3c_.js
@@ -0,0 +1,2 @@
+import{i as e}from"./vendor-C-zllbUV.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
+//# sourceMappingURL=browser-Db2dz3c_.js.map
