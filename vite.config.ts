57c1e6ef1c9e5b7b767d5cd3b1ed869f157a914a
Jefferson <hbrcomercialssa@gmail.com>
2025-06-11 21:55:01 -0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/simulados-concursos-expert/",
   server: {
     port: 8080,
     host: true,
@@ -17,7 +17,7 @@ export default defineConfig({
     },
   },
   build: {
-    outDir: "dist",
+    outDir: "docs",
     emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
